c49083e433488c4a19cecc3c5048cba7e8b47751
Zohaib <zohaibb@ntnu.no>
2018-11-11 21:08:42 +0100

    Need to run bruteforce_stack.sh

--- dash_shell_test.c
+++ dash_shell_test.c
@@ -7,7 +7,9 @@ int main(){
 char *argv[2];
 argv[0] = "/bin/sh";
 argv[1] = NULL;
+printf("Before;%i\n", getuid());
 setuid(0);
+printf("After:%i\n", getuid());
 
 execve("/bin/sh", argv, NULL);
 return 0;
